e34aa2d7c2ec9c7e4897c5f384bf6d06bba8f80c
Manav Kothari <manavkothari11@gmail.com>
2016-06-23 11:13:56 -0400

    adding the upadted ImageRequest project.

--- ImageRequest/ImageRequest/ViewController.swift
+++ ImageRequest/ImageRequest/ViewController.swift
@@ -20,6 +20,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let imageURL = NSURL( string: "http://www.atpworldtour.com/en/news/www.atpworldtour.com/~/media/images/news/2016/01/07/12/28/federer-brisbane-2016-thursday2.jpg")!
+        let task = NSURLSession.sharedSession().dataTaskWithURL(imageURL) { (data, response, error) -> Void in
+            if error==nil {
+                let downloadedImage = UIImage(data: data!)
+                performUIUpdatesOnMain{
+                    self.imageView.image = downloadedImage
+                }
+            }
+            
+        }
+        task.resume()
         
         // TODO: Add all the networking code here!
     }
